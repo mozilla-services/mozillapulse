3891b575e90786d6960b19376dc2bb23b9f6c5c1
Daniel Brooks <db48x@db48x.net>
2011-01-10 17:12:57 -0600

    fix the HgConsumer so that it actually specifies an exchange to listen to

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup
+
+setup(name='MozillaPulse',
+      version='.4',
+      description='Helper library for interacting with the Mozilla Pulse ' +
+                  'message system at pulse.mozilla.org',
+      author='Christian Legnitto',
+      author_email='clegnitto@mozilla.com',
+      packages=['mozillapulse','mozillapulse.messages'],
+      install_requires=['carrot', 'pytz'],
+)
