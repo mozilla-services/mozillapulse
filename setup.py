ba1d94415d308f59def20fe509dae38f1ab517b3
Mark Cote <mcote@mozilla.com>
2013-06-20 16:09:27 -0400

    Bug 623305 - Proper version bump. r=jgriffin

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='MozillaPulse',
-      version='0.7',
+      version='0.70',
       description='Helper library for interacting with the Mozilla Pulse ' +
                   'message system at pulse.mozilla.org',
       author='Mark Cote',
