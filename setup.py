6bd2f2a6942704ef06baf22177f901bf8c5f95ad
Mark Cote <mcote@mozilla.com>
2015-07-27 14:38:21 -0400

    Bump version to 1.1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='MozillaPulse',
-      version='1.0.1',
+      version='1.1',
       description='Helper library for interacting with the Mozilla Pulse ' +
                   'message system at pulse.mozilla.org',
       url='https://hg.mozilla.org/automation/mozillapulse/',
